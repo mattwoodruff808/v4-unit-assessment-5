e3fa9bb4e553594aa8b462d6979bfc206c5406c4
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-19 15:40:59 -0700

    Finished the Login and Logout endpoints

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -17,11 +17,28 @@ module.exports = {
         req.session.user = newUser;
         res.status(201).send(req.session.user);
     },
-    login: (req, res) => {
+    login: async (req, res) => {
+        const {username, password} = req.body;
+        const db = req.app.get('db');
 
+        const [foundUser] = await db.user.find_user_by_username({username});
+        if (!foundUser){
+            return res.status(404).send('Username not found');
+        }
+
+        const authenticated = bcrypt.compareSync(password, foundUser.password);
+        if (!authenticated){
+            return res.status(401).send('Password is incorrect');
+        }
+
+        delete foundUser.password;
+
+        req.session.user = foundUser;
+        res.status(202).send(req.session.user);
     },
     logout: (req, res) => {
-
+        req.session.destroy();
+        res.sendStatus(200);
     },
     getUser: (req, res) => {
 
