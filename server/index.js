2c0d318597d135c31295195fc95d93c0f7710f25
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-19 11:49:10 -0700

    Finished up to Authentication, about to start

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 const express = require('express'),
+      massive = require('massive'),
       userCtrl = require('./controllers/user'),
-      postCtrl = require('./controllers/posts')
+      postCtrl = require('./controllers/posts'),
+      {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
 
 
 const app = express();
 
 app.use(express.json());
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db);
+    console.log('db connected');
+})
 
 //Auth Endpoints
 app.post('/api/auth/register', userCtrl.register);
@@ -20,4 +30,4 @@ app.post('/api/post', postCtrl.createPost);
 app.get('/api/post/:id', postCtrl.readPost);
 app.delete('/api/post/:id', postCtrl.deletePost)
 
-app.listen(4000, _ => console.log(`running on ${4000}`));
+app.listen(SERVER_PORT, _ => console.log(`running on ${SERVER_PORT}`));
