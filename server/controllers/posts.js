b5dedf84fdef84d8fd5b4321df433f0d3a485def
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-19 19:42:02 -0700

    Finished Routing section

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -37,8 +37,18 @@ module.exports = {
         }
       }
     },
-    createPost: (req, res) => {
-      //code here
+    createPost: async (req, res) => {
+      const db = await req.app.get('db');
+      const {id} = req.session.user;
+      const {title, img, content} = req.body;
+      const date = new Date;
+
+      if (!id){
+        return res.sendStatus(403);
+      }
+
+      db.post.create_post([id, title, img, content, date]);
+      res.sendStatus(200);
     },
     readPost: (req, res) => {
       req.app.get('db').post.read_post(req.params.id)
